65c52d87028adae0eda989a049bb2125e3d9970b
Juliano Melo <julianomelorodriguesalencar@gmail.com>
2025-06-09 20:37:19 -0300

    Adicionando os models

--- Interface_OCI/core/models.py
+++ Interface_OCI/core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Escola(models.Model):
+    nome = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.nome
+    
+class Participante(models.Model):
+    id_participante = models.IntegerField(unique=True)
+    nome = models.CharField(max_length=64)
+    escola = models.ForeignKey(Escola, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
+
+class Prova(models.Model):
+    id_prova = models.IntegerField(unique=True)
+    gabarito = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f'Prova {self.id_prova} - Gabarito: {self.gabarito}'
+    
