6bd5bf492ecb03ffb68993b7ecea5a921ee6e768
Juliano Melo <julianomelorodriguesalencar@gmail.com>
2025-06-10 13:50:17 -0300

    Views e Forms

--- Interface_OCI/core/views.py
+++ Interface_OCI/core/views.py
@@ -1,6 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Escola, Participante, Prova
+from .forms import ParticipanteForm, ProvaForm, EscolaForm
 
 # Este arquivo define as views da aplicação Interface_OCI.
 # As views são responsáveis por processar as requisições HTTP e retornar as respostas apropriadas.
 
-# Create your views here.
+def lista_escolas(request):
+    escolas = Escola.objects.all()
+    return render(request, 'core/lista_escolas.html', {'escolas': escolas})
+
+def lista_participantes(request):
+    participantes = Participante.objects.all()
+    return render(request, 'core/lista_participantes.html', {'participantes': participantes})
+
+def lista_provas(request):
+    provas = Prova.objects.all()
+    return render(request, 'core/lista_provas.html', {'provas': provas})
+
+def cadastrar_participante(request):
+    if request.method == 'POST':
+        form = ParticipanteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_participantes')
+    else:
+        form = ParticipanteForm()
+    return render(request, 'core/cadastrar_participante.html', {'form': form})
+
+def cadastrar_escola(request):
+    if request.method == 'POST':
+        form = EscolaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_escolas')
+    else:
+        form = EscolaForm()
+    return render(request, 'core/cadastrar_escola.html', {'form': form})
+
+def cadastrar_prova(request):
+    if request.method == 'POST':
+        form = ProvaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_provas')
+    else:
+        form = ProvaForm()
+    return render(request, 'core/cadastrar_prova.html', {'form': form})
