b67b3194b65095fec2ff30dfc31752fcaef58a5d
Juliano Melo <julianomelorodriguesalencar@gmail.com>
2025-06-10 21:35:04 -0300

    Alterando a lógica dos id's

--- Interface_OCI/core/models.py
+++ Interface_OCI/core/models.py
@@ -1,17 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Este arquivo define os modelos do Django para a aplicação Interface_OCI.
 # Os modelos representam as tabelas do banco de dados e são usados para interagir com os dados.
 # Importante: Certifique-se de que o Django esteja configurado corretamente para usar este modelo.
 # Definindo os modelos para a aplicação Interface_OCI
 class Escola(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     nome = models.CharField(max_length=64)
 
     def __str__(self):
         return self.nome
     
 class Participante(models.Model):
-    id_participante = models.IntegerField(unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     nome = models.CharField(max_length=64)
     escola = models.ForeignKey(Escola, on_delete=models.CASCADE)
 
@@ -19,8 +21,8 @@ class Participante(models.Model):
         return self.nome
 
 class Prova(models.Model):
-    id_prova = models.IntegerField(unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     gabarito = models.CharField(max_length=20)
 
     def __str__(self):
-        return f'Prova {self.id_prova} - Gabarito: {self.gabarito}'
+        return f'Prova {self.id} - Gabarito: {self.gabarito}'
--- Interface_OCI/core/views.py
+++ Interface_OCI/core/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from .models import Escola, Participante, Prova
 from .forms import ParticipanteForm, ProvaForm, EscolaForm
+from django.db.models import Max
 
 # Este arquivo define as views da aplicação Interface_OCI.
 # As views são responsáveis por processar as requisições HTTP e retornar as respostas apropriadas.
@@ -25,17 +26,17 @@ def index(request):
 
 @login_required
 def lista_escolas(request):
-    escolas = Escola.objects.all()
+    escolas = Escola.objects.filter(user=request.user)
     return render(request, 'core/lista_escolas.html', {'escolas': escolas})
 
 @login_required
 def lista_participantes(request):
-    participantes = Participante.objects.all()
+    participantes = Participante.objects.filter(user=request.user)
     return render(request, 'core/lista_participantes.html', {'participantes': participantes})
 
 @login_required
 def lista_provas(request):
-    provas = Prova.objects.all()
+    provas = Prova.objects.filter(user=request.user)
     return render(request, 'core/lista_provas.html', {'provas': provas})
 
 @login_required
@@ -43,7 +44,9 @@ def cadastrar_participante(request):
     if request.method == 'POST':
         form = ParticipanteForm(request.POST)
         if form.is_valid():
-            form.save()
+            participante = form.save(commit=False)
+            participante.user = request.user
+            participante.save()
             return redirect('lista_participantes')
     else:
         form = ParticipanteForm()
@@ -54,7 +57,9 @@ def cadastrar_escola(request):
     if request.method == 'POST':
         form = EscolaForm(request.POST)
         if form.is_valid():
-            form.save()
+            escola = form.save(commit=False)
+            escola.user = request.user
+            escola.save()
             return redirect('lista_escolas')
     else:
         form = EscolaForm()
@@ -65,7 +70,9 @@ def cadastrar_prova(request):
     if request.method == 'POST':
         form = ProvaForm(request.POST)
         if form.is_valid():
-            form.save()
+            prova = form.save(commit=False)
+            prova.user = request.user
+            prova.save()
             return redirect('lista_provas')
     else:
         form = ProvaForm()
