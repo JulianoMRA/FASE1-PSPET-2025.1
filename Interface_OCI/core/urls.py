099f62d39fa746a943d7453f761fd3c7c49e2d19
Juliano Melo <julianomelorodriguesalencar@gmail.com>
2025-06-12 23:38:41 -0300

    ...

--- Interface_OCI/core/urls.py
+++ Interface_OCI/core/urls.py
@@ -37,6 +37,7 @@ urlpatterns = [
     # Autenticação e cadastro de usuário
     # -------------------------
     path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', views.signup, name='signup'),
 
     # -------------------------
